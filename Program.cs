0ef2edac0b450ddbcc94b9ab27d35a5d0d0cc000
Alex Isaak <gafar@xaker.ru>
2023-02-01 19:55:32 +0300

    itogovy script

--- Program.cs
+++ Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+ 
+namespace dni
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите номер дня недели: ");
+            int dayindex = int.Parse(Console.ReadLine());
+             
+    switch ( dayindex ) {
+        case 1 :
+            Console.WriteLine( "Понедельник" );
+            break;
+        case 2 :
+            Console.WriteLine( "Вторник" );
+            break;
+        case 3 :
+            Console.WriteLine( "Среда" );
+            break;
+        case 4 :
+            Console.WriteLine( "Четверг" );
+            break;
+        case 5 :
+            Console.WriteLine( "Пятница" );
+            break;
+        case 6 :
+            Console.WriteLine( "Суббота" );
+            break;
+        case 7 :
+            Console.WriteLine( "Воскресенье" );
+            break;
+        default :
+            Console.WriteLine( "Неожиданный номер дня недели" );
+            break;
+      
+        }
+    }
+}
+}
